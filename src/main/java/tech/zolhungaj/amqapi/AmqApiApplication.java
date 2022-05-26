f3cc5e7a20d7f2d721bc8f53205e25142e4d9e8c
Zolhungaj <zolhungaj@gmail.com>
2022-05-26 14:03:36 +0200

    initial commit

---
+++ src/main/java/tech/zolhungaj/amqapi/AmqApiApplication.java
@@ -0,0 +1,13 @@
+package tech.zolhungaj.amqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/zolhungaj/amqapi/AmqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zolhungaj.amqapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
