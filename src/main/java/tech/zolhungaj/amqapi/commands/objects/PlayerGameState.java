3644d429538042538124b839dfea40e60076a2c7
Zolhungaj <zolhungaj@gmail.com>
2022-07-03 17:20:31 +0200

    add isRankedGame and isSoloGame to PlayerGameState

--- src/main/java/tech/zolhungaj/amqapi/commands/objects/PlayerGameState.java
+++ src/main/java/tech/zolhungaj/amqapi/commands/objects/PlayerGameState.java
@@ -1,10 +1,22 @@
 package tech.zolhungaj.amqapi.commands.objects;
 
 import com.squareup.moshi.Json;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public record PlayerGameState (
-        Integer gameId,
+        Optional<Integer> gameId,
         Boolean isSpectator,
         @Json(name = "private") Boolean isPrivateRoom,
+        @Json(name = "soloGame") @Nullable Boolean isSoloGame,
+        @Json(name = "isRanked") @Nullable Boolean isRankedGame,
         Boolean inLobby
-){}
+){
+    public Optional<Boolean> isSoloGameOptional(){
+        return Optional.ofNullable(isSoloGame);
+    }
+    public Optional<Boolean> isRankedGameOptional(){
+        return Optional.ofNullable(isRankedGame);
+    }
+}
