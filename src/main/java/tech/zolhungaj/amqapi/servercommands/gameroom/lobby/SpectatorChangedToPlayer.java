8a91f3a8349f63d872c85921c8cc4cc29d30c3eb
Zolhungaj <zolhungaj@gmail.com>
2023-03-27 21:19:20 +0200

    make SpectatorChangedToPlayer a record instead of a weird extension of something else

--- src/main/java/tech/zolhungaj/amqapi/servercommands/gameroom/lobby/SpectatorChangedToPlayer.java
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/gameroom/lobby/SpectatorChangedToPlayer.java
@@ -1,11 +1,22 @@
 package tech.zolhungaj.amqapi.servercommands.gameroom.lobby;
 
+import com.squareup.moshi.Json;
 import tech.zolhungaj.amqapi.servercommands.Command;
 import tech.zolhungaj.amqapi.servercommands.CommandType;
-import tech.zolhungaj.amqapi.servercommands.objects.Player;
+import tech.zolhungaj.amqapi.servercommands.objects.PlayerAvatar;
 
+import java.util.Optional;
 
-public final class SpectatorChangedToPlayer extends Player implements Command {
+public record SpectatorChangedToPlayer(
+        @Json(name = "name")
+        String playerName,
+        int gamePlayerId,
+        int level,
+        PlayerAvatar avatar,
+        boolean ready,
+        boolean inGame,
+        Optional<Integer> teamNumber
+) implements Command {
     @Override
     public String commandName() {
         return CommandType.SPECTATOR_CHANGED_TO_PLAYER.commandName;
--- src/main/java/tech/zolhungaj/amqapi/servercommands/objects/Player.java
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/objects/Player.java
@@ -1,18 +1,16 @@
 package tech.zolhungaj.amqapi.servercommands.objects;
 
 import com.squareup.moshi.Json;
-import lombok.Data;
-import org.jetbrains.annotations.Nullable;
 
-@Data
-public class Player {
-    @Json(name = "name")
-    private String playerName;
-    private Integer gamePlayerId;
-    private Integer level;
-    private PlayerAvatar avatar;
-    private Boolean ready;
-    private Boolean inGame;
-    @Nullable
-    private Integer teamNumber;
-}
+import java.util.Optional;
+
+public record Player(
+        @Json(name = "name")
+        String playerName,
+        int gamePlayerId,
+        int level,
+        PlayerAvatar avatar,
+        boolean ready,
+        boolean inGame,
+        Optional<Integer> teamNumber
+) {}
