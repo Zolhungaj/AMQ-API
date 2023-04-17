77a09f3375d4c9d39175be137f09658da73cf2af
Zolhungaj <zolhungaj@gmail.com>
2023-04-18 00:17:25 +0200

    fix PlayerLeft
    no more nulls

--- src/main/java/tech/zolhungaj/amqapi/servercommands/gameroom/PlayerLeft.java
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/gameroom/PlayerLeft.java
@@ -7,11 +7,16 @@ import tech.zolhungaj.amqapi.servercommands.objects.PlayerIdentifier;
 import java.util.Optional;
 
 public record PlayerLeft(
-        Optional<Boolean> kicked,
-        Optional<Boolean> disconnect,
+        Boolean kicked,
+        Boolean disconnect,
         Optional<String> newHost,
         PlayerIdentifier player
 ) implements Command {
+    public PlayerLeft{
+        if(kicked == null) kicked = false;
+        if(disconnect == null) disconnect = false;
+        if(newHost == null) newHost = Optional.empty();
+    }
     @Override
     public String commandName() {
         return CommandType.PLAYER_LEFT.commandName;
