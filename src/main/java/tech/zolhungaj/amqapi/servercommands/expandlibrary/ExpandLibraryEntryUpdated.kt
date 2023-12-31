96ab7595fb58914b85980aa9311b3222dfe5033a
Zolhungaj <zolhungaj@gmail.com>
2023-12-31 20:18:35 +0100

    convert ExpandLibraryEntryUpdated to kotlin

--- src/main/java/tech/zolhungaj/amqapi/servercommands/expandlibrary/ExpandLibraryEntryUpdated.kt
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/expandlibrary/ExpandLibraryEntryUpdated.kt
@@ -1,16 +1,19 @@
-package tech.zolhungaj.amqapi.servercommands.expandlibrary;
+package tech.zolhungaj.amqapi.servercommands.expandlibrary
 
-import com.squareup.moshi.Json;
-import tech.zolhungaj.amqapi.servercommands.CommandType;
+import com.squareup.moshi.Json
+import tech.zolhungaj.amqapi.servercommands.CommandType
 
-import java.util.Optional;
-
+@JvmRecord
 @CommandType("expandLibrary song answered")
-public record ExpandLibraryEntryUpdated(
-	int annId,
-	int annSongId,
-	String host,
-	int resolution,
-	@Json(name = "animeStillAvaliable") Boolean animeStillAvailable,
-	@Json(name = "songStillAvaliable") Optional<Boolean> songStillAvailable
-) {}
+data class ExpandLibraryEntryUpdated(
+    @Json(name = "annId")
+    val animeNewsNetworkId: Int,
+    @Json(name = "annSongId")
+    val songId : Int,
+    val host: String,
+    val resolution: Int,
+    @Json(name = "animeStillAvaliable")
+    val animeStillAvailable : Boolean,
+    @Json(name = "songStillAvaliable")
+    val songStillAvailable : Boolean?
+)
