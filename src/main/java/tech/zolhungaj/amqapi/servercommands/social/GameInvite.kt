4c94e94e60639a687cb880819b6505d90987527e
Zolhungaj <zolhungaj@gmail.com>
2024-01-01 21:24:20 +0100

    fix nullability

--- src/main/java/tech/zolhungaj/amqapi/servercommands/social/GameInvite.kt
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/social/GameInvite.kt
@@ -5,6 +5,6 @@ import tech.zolhungaj.amqapi.servercommands.CommandType
 @JvmRecord
 @CommandType("game invite")
 data class GameInvite(
-    val gameId: Int,
+    val gameId: Int?,
     val sender: String
 )
