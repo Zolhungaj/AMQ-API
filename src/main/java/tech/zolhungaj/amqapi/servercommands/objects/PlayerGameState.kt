daa77121726da1458b55938f76ef835e47120481
Zolhungaj <zolhungaj@gmail.com>
2024-01-03 20:11:18 +0100

    replace Optional with nullable

--- src/main/java/tech/zolhungaj/amqapi/servercommands/objects/PlayerGameState.kt
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/objects/PlayerGameState.kt
@@ -1,11 +1,10 @@
 package tech.zolhungaj.amqapi.servercommands.objects
 
 import com.squareup.moshi.Json
-import java.util.*
 
 @JvmRecord
 data class PlayerGameState(
-    val gameId: Optional<Int>,
+    val gameId: Int?,
     val isSpectator: Boolean,
     @Json(name = "private") val isPrivateRoom: Boolean,
     @Json(name = "soloGame") val isSoloGame: Boolean?,
