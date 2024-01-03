5ddc058e6f4ca27cb74d996b47e1ef30c32066e4
Zolhungaj <zolhungaj@gmail.com>
2024-01-03 20:07:28 +0100

    add activeGames object to RankedGameStateChanged

--- src/main/java/tech/zolhungaj/amqapi/servercommands/globalstate/RankedGameStateChanged.kt
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/globalstate/RankedGameStateChanged.kt
@@ -4,10 +4,12 @@ import com.squareup.moshi.Json
 import tech.zolhungaj.amqapi.constants.RankedSeries
 import tech.zolhungaj.amqapi.constants.RankedState
 import tech.zolhungaj.amqapi.servercommands.CommandType
+import tech.zolhungaj.amqapi.servercommands.objects.ActiveRankedGameModes
 
 @JvmRecord
 @CommandType("ranked game state change")
 data class RankedGameStateChanged(
     @Json(name = "serieId") val rankedSeries: RankedSeries,
-    @Json(name = "state") val state: RankedState
+    @Json(name = "state") val state: RankedState,
+    @Json(name = "games") val activeGames: ActiveRankedGameModes,
 )
