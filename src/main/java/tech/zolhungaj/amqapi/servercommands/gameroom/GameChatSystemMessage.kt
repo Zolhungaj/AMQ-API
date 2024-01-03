3a25eea294f7bd7124400036537e70910f2a3a34
Zolhungaj <zolhungaj@gmail.com>
2024-01-03 21:41:15 +0100

    rewrite to match new format

--- src/main/java/tech/zolhungaj/amqapi/servercommands/gameroom/GameChatSystemMessage.kt
+++ src/main/java/tech/zolhungaj/amqapi/servercommands/gameroom/GameChatSystemMessage.kt
@@ -6,9 +6,5 @@ import tech.zolhungaj.amqapi.servercommands.CommandType
 @JvmRecord
 @CommandType("Game System Chat Message")
 data class GameChatSystemMessage(
-    val title: String,
-    @Json(name = "msg")
-    val message: String?,
-    @Json(name = "teamMessage")
-    val isTeamMessage: Boolean
+    @Json(name = "message") val message: String
 )
