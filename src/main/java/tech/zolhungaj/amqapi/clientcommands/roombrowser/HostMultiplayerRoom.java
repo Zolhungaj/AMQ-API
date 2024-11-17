add5d037a917a4bfa071d0874c6dccc7dce2333d
Zolhungaj <Zolhungaj@gmail.com>
2024-11-17 19:17:52 +0100

    fix game settings commands

--- src/main/java/tech/zolhungaj/amqapi/clientcommands/roombrowser/HostMultiplayerRoom.java
+++ src/main/java/tech/zolhungaj/amqapi/clientcommands/roombrowser/HostMultiplayerRoom.java
@@ -1,17 +1,19 @@
 package tech.zolhungaj.amqapi.clientcommands.roombrowser;
 
 import tech.zolhungaj.amqapi.clientcommands.CommandName;
-import tech.zolhungaj.amqapi.clientcommands.DirectDataCommand;
 import tech.zolhungaj.amqapi.sharedobjects.gamesettings.GameSettings;
 
-@DirectDataCommand("settings")
 @CommandName("host room")
 public record HostMultiplayerRoom(
-        GameSettings settings
+        GameSettings settings,
+        Boolean communityMode
 ) implements RoomBrowserCommand {
     public HostMultiplayerRoom {
         var settingsBuilder = settings.toBuilder();
         settingsBuilder.gameMode(GameSettings.GameMode.MULTIPLAYER.value);
         settings = settingsBuilder.build();
+    }
+    public HostMultiplayerRoom(GameSettings settings) {
+        this(settings, false);
     }
 }
